b10d0227194f40493c4df24b9259734a6b24ae2d
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-06 23:36:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,23 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do 
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirates]) 
+
+      params[:pirates][:ships].each do |details|
+        Ship.new(details)
+      end
+        @ships = Ship.all
+      erb :'pirates/show'
+    end
+
   end
 end
